ed02b26e6c7deba232310960d6e2ff38d28e76b1
xschen <xs0040@gmail.com>
2017-06-17 22:03:48 +0800

    unit testing for the replacement and crossover

--- src/test/java/com/github/chen0040/moea/algorithms/NSGAIIUnitTest.java
+++ src/test/java/com/github/chen0040/moea/algorithms/NSGAIIUnitTest.java
@@ -2,6 +2,8 @@ package com.github.chen0040.moea.algorithms;
 
 
 import com.github.chen0040.moea.components.NondominatedPopulation;
+import com.github.chen0040.moea.enums.CrossoverType;
+import com.github.chen0040.moea.enums.ReplacementType;
 import com.github.chen0040.moea.tutorials.NDND;
 import com.github.chen0040.moea.tutorials.TNK;
 import org.testng.annotations.Test;
@@ -23,9 +25,31 @@ public class NSGAIIUnitTest {
       algorithm.setDisplayEvery(10);
 
       NondominatedPopulation pareto_front = algorithm.solve();
+   }
 
+   @Test
+   public void test_ndnd_crossover_uniform(){
+      NSGAII algorithm = new NSGAII();
+      algorithm.getMediator().read(new TNK());
+      algorithm.getMediator().setCrossoverType(CrossoverType.Uniform);
+      algorithm.getMediator().setPopulationSize(1000);
+      algorithm.getMediator().setMaxGenerations(100);
+      algorithm.setDisplayEvery(10);
 
+      NondominatedPopulation pareto_front = algorithm.solve();
 
+   }
+
+   @Test
+   public void test_ndnd_replacement_tournament(){
+      NSGAII algorithm = new NSGAII();
+      algorithm.getMediator().read(new TNK());
+      algorithm.getMediator().setReplacementType(ReplacementType.Tournament);
+      algorithm.getMediator().setPopulationSize(1000);
+      algorithm.getMediator().setMaxGenerations(100);
+      algorithm.setDisplayEvery(10);
+
+      NondominatedPopulation pareto_front = algorithm.solve();
    }
 
    @Test
